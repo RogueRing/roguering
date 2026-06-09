08adad24e96667f961ada01e4ae7737f9e12fdfa
RogueRing <291063140+RogueRing@users.noreply.github.com>
2026-06-09 17:20:01 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -10,6 +10,8 @@ const supabaseClient =
         SUPABASE_KEY
     );
 
+delete L.Icon.Default.prototype._getIconUrl;
+
 L.Icon.Default.mergeOptions({
     iconRetinaUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
     iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
